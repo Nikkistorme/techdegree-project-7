ef5f3e9ff343c5e8c722f928a648335bc110570d
Nicholas Bright <nikkistorme@gmail.com>
2019-04-06 16:18:41 -0500

    Finished from 320px-767px

--- js/app.js
+++ js/app.js
@@ -40,7 +40,6 @@ const trafficChart = new Chart(chart1, {
     }]
   },
   options: {
-    maintainAspectRatio: false,
     responsive: true,
     legend: {
       display: false
@@ -90,7 +89,6 @@ const dailyTrafficChart = new Chart(chart2, {
     ]
   },
   options: {
-    maintainAspectRatio: false,
     responsive: true,
     legend: { display: false },
     scales: {
@@ -121,7 +119,6 @@ const mobileUsersChart = new Chart(chart3, {
     }]
   },
   options: {
-    maintainAspectRatio: false,
     responsive: true,
     legend: {
       position: 'right',
