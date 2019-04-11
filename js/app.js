729228ba4e64ca556a1fe6da03f6ccf277b4294e
Nicholas Bright <nikkistorme@gmail.com>
2019-04-11 11:24:14 -0500

    notifs, alert, autocomplete, dynamnic graph -check

--- js/app.js
+++ js/app.js
@@ -5,26 +5,49 @@
 const alertBanner = document.getElementById('alert-banner'); // retrieve banner
 const alertClearButton = document.getElementById('alert-clear-button'); // retrieve clear button
 
-alertClearButton.addEventListener('click', (e) => { // listen for clicks of clear button
+alertClearButton.addEventListener('click', () => { // listen for clicks of clear button
     alertBanner.style.display = 'none'; // hide alert banner
+})
+
+// Notifications
+const bell = document.getElementById('bell');
+const bellCircle = document.getElementById('bell-circle');
+const notifsBox = document.getElementById('notifs-box');
+const notifsBoxArray = Array.from(notifsBox.childNodes);
+const notifs = Array.from(document.getElementsByClassName('notif'))
+
+bell.addEventListener('click', () => {
+    bellCircle.style.display = 'none';
+    notifsBox.style.display = 'grid';
+})
+
+notifsBox.addEventListener('click', (e) => {
+    if (e.target.className === 'notif-clear-all fas fa-times') {
+        notifsBox.style.display = 'none';
+    }
+    if (e.target.className === 'notif-clear fas fa-times') {
+        const notif = e.target.parentNode.parentNode;
+        notif.style.display = 'none';
+    }
 })
 
 // SIDEBAR
 const nav = document.getElementById('navigation'); // retrieve navigation
-const navLinks = nav.getElementsByClassName('nav-link'); // retrieve navigation links
+let navLinks = nav.getElementsByClassName('nav-link'); // retrieve navigation links
 
 nav.addEventListener('click', (e) => { // listen for clicks in navigation bar
     for (i = 0; i < navLinks.length; i += 1) { // go through navLinks,
         navLinks[i].classList.remove('active'); // and remove all .active classes
     }
     if (e.target.className !== 'active') { // if link does not already have .active,
-        e.target.className = 'active';
+        e.target.classList.add('active'); // add .active
     }
 })
 
 
 // USER MESSAGES
 
+// alerts
 const messageUser = document.getElementById('message-user'); // retrieve user search box
 const messageContent = document.getElementById('message-content'); // retrieve message box
 const messageSubmit = document.getElementById('message-submit'); // retreive message submit button
@@ -40,3 +63,98 @@ messageSubmit.addEventListener('click', (e) => { // listen for when the form is 
         alert('Your message to ' + messageUser.value + ' was sent!');
     }
 })
+
+// searching for user - autocomplete
+const users = ['Andy','Bill','Carrie','Dale','Dawn','Dan','Earnest','Fred','Gregory','Holly','Inigo','Jenny','Kenny','Libby','Mindy','Nikki','Onobe','Penny','Quinn','Randall','Sandy','Tom','Umbrasil','Victoria','Wendy','Xavier','Yvette','Zander'];
+const input = document.getElementById('message-user');
+let inputVal = '';
+
+function autocomplete(val) {
+    let usersReturn = [];
+    for (i = 0; i < users.length; i += 1) {
+        if (val.toLowerCase() === users[i].toLowerCase().slice(0, val.length)) {
+            usersReturn.push(users[i]);
+        }
+    }
+    return usersReturn;
+}
+
+input.addEventListener('keyup', (e) => {
+    const key = e.key.toString();
+    const autocompleteResults = document.getElementById('autocomplete-results');
+    let usersToShow = [];
+    if (key !== 'Backspace') {
+        inputVal += key;
+    } else if (key === 'Backspace') {
+        inputVal = inputVal.slice(0, -1);
+    }
+    if (inputVal.length > 0) {
+        autocompleteResults.innerHTML = '';
+        usersToShow = autocomplete(inputVal);
+        for (i = 0; i < usersToShow.length; i++) {
+            autocompleteResults.innerHTML += '<li>' + usersToShow[i] + '</li>';
+        }
+        autocompleteResults.style.position = 'absolute';
+        autocompleteResults.style.display = 'block';
+    } else {
+        usersToShow = [];
+        autocompleteResults.innerHTML = '';
+    }
+})
+
+
+// LOCAL STORAGE
+const emailSwitch = document.getElementById("switch-1");
+const publicSwitch = document.getElementById("switch-2");
+const timezone = document.getElementById("timezone");
+const submit = document.getElementById("settings-submit");
+const reset = document.getElementById("settings-cancel");
+
+function supportsLocalStorage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch(e) {
+        return false;
+    }
+}
+
+function saveSettings() {
+    localStorage.setItem('emailSwitch', emailSwitch.checked);
+    localStorage.setItem('publicSwitch', publicSwitch.checked);
+    localStorage.setItem('timezone', timezone[timezone.selectedIndex].value);
+  }
+  
+  function loadSettings() {
+    let email = JSON.parse(localStorage.getItem('emailSwitch'));
+    emailSwitch.checked = email;
+    let public = JSON.parse(localStorage.getItem('publicSwitch'));
+    publicSwitch.checked = public;
+    let selectedTimezone = localStorage.getItem('timezone');
+    timezone.value = selectedTimezone;
+  }
+  
+  function resetSettings() {
+    // location.reload();
+    localStorage.clear();
+    localStorage.setItem('timezone', timezone.value = "Your timezone");
+  }
+  
+  submit.addEventListener('click', (e) => {
+    if(e.target.type === "submit") {
+        saveSettings();
+        e.preventDefault();
+    }
+  });
+  
+  reset.addEventListener('click', (e) => {
+    if(e.target.type === "reset") {
+        resetSettings();
+    }
+  });
+  
+  
+  window.onload = function() {
+      if (supportsLocalStorage) {
+        loadSettings();
+      }
+  };
--- js/my-charts.js
+++ js/my-charts.js
@@ -2,74 +2,108 @@
 // CHARTS
 
 // trafficChart
+const hourlyLabels = ['8:00a','9:00a','10:00a','11:00a','12:00p','1:00p','2:00p','3:00p','4:00p','5:00p','6:00p'];
+const hourlyData = ['5','8','7','12','15','16','17','10','13','2','20'];
+const hourlyStepSize = 5;
+const hourlyStepMax = 25;
+const dailyLabels = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const dailyData = ['75','120','150','200','100','235','142'];
+const dailyStepSize = 50;
+const dailyStepMax = 250;
+const weeklyLabels = ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'];
+const weeklyData = [500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500];
+const weeklyStepSize = 500;
+const weeklyStepMax = 2500;
+const monthlyLabels = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+const monthlyData = ['5000','3000','10000','12000','7000','6000','5500','15000','17500','20000','13000','9000'];
+const monthlyStepSize = 5000;
+const monthlyStepMax = 25000;
+
 const chart1 = document.getElementById('trafficChart');
-const trafficChart = new Chart(chart1, {
-  type: 'line',
-  data: {
-    labels: [
-      '',
-      '16-22',
-      '23-29',
-      '30-5',
-      '6-12',
-      '13-19',
-      '20-26',
-      '27-3',
-      '4-10',
-      '11-17',
-      '18-24',
-      '25-31'],
-    datasets: [{
-      data: [
-        0,
-        500,
-        1000,
-        750,
-        1250,
-        1750,
-        1250,
-        1500,
-        1000,
-        1500,
-        2000,
-        1500
-      ],
-      backgroundColor: 'rgba(226, 227, 246, 0.7)',
-      borderColor: 'rgba(116, 119, 191, 1)',
-      borderWidth: .5
-    }]
-  },
-  options: {
-    maintainAspectRatio: false,
-    responsive: true,
-    legend: {
-      display: false
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-          stepSize: 500,
-          max: 2500,
-        }
-      }],
-      xAxes: [{
-        ticks: {
-          maxTicksLimit: 12
-        }
+function createLineChart(labels, data, stepSize, stepMax) {
+  const trafficChart = new Chart(chart1, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [{
+        data: data,
+        backgroundColor: 'rgba(226, 227, 246, 0.7)',
+        borderColor: 'rgba(116, 119, 191, 1)',
+        borderWidth: .5
       }]
     },
-    elements: {
-      line: {
-        tension: 0
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      legend: {
+        display: false
       },
-      point: {
-        radius: 5,
-        backgroundColor: 'rgba(0, 0, 0, 0)'
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            stepSize: stepSize,
+            max: stepMax,
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            maxTicksLimit: 12
+          }
+        }]
+      },
+      elements: {
+        line: {
+          tension: 0
+        },
+        point: {
+          radius: 5,
+          backgroundColor: 'rgba(0, 0, 0, 0)'
+        }
       }
     }
+  })  
+};
+
+createLineChart(weeklyLabels, weeklyData, weeklyStepSize, weeklyStepMax);
+
+
+// adjust traffic chart based on navigation
+const trafficNav = document.getElementById('traffic-chart-nav');
+let trafficLinks = document.getElementsByClassName('traffic-link');
+
+trafficNav.addEventListener('click', (e) => {
+  if (e.target.textContent === 'Hourly') {
+    createLineChart(hourlyLabels, hourlyData, hourlyStepSize, hourlyStepMax);
+    for (i = 0; i < trafficLinks.length;i += 1) {
+      trafficLinks[i].parentNode.parentNode.className = '';
+    }
+    e.target.parentNode.parentNode.className = "active";
+  }
+  if (e.target.textContent === 'Daily') {
+    createLineChart(dailyLabels, dailyData, dailyStepSize, dailyStepMax);
+    for (i = 0; i < trafficLinks.length;i += 1) {
+      trafficLinks[i].parentNode.parentNode.className = '';
+    }
+    e.target.parentNode.parentNode.className = "active";
+  }
+  if (e.target.textContent === 'Weekly') {
+    createLineChart(weeklyLabels, weeklyData, weeklyStepSize, weeklyStepMax);
+    for (i = 0; i < trafficLinks.length;i += 1) {
+      trafficLinks[i].parentNode.parentNode.className = '';
+    }
+    e.target.parentNode.parentNode.className = "active";
+  }
+  if (e.target.textContent === 'Monthly') {
+    createLineChart(monthlyLabels, monthlyData, monthlyStepSize, monthlyStepMax);
+    for (i = 0; i < trafficLinks.length;i += 1) {
+      trafficLinks[i].parentNode.parentNode.className = '';
+    }
+    e.target.parentNode.parentNode.className = "active";
   }
 })
+
+
 
 // DAILY TRAFFIC BAR CHART
 const chart2 = document.getElementById('dailyTrafficChart');
